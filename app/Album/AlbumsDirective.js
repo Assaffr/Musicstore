1e4346d3fc8edfbea5663e19238b8d461c33df43
shani28s5 <shani28s5@gmail.com>
2015-11-03 16:46:20 +0200

    figuringshitout

--- app/Album/AlbumsDirective.js
+++ app/Album/AlbumsDirective.js
@@ -1,4 +1,4 @@
-AlbumsModule.directive( 'centerd-top-nav', function() {
+AlbumsModule.directive( 'centerdtopnav', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'app/album/_centerd-top-nav.html'
