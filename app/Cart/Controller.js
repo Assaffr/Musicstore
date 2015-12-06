b3a29637348534d5455cad3c809f9748f5164e4d
shani28s5 <shani28s5@gmail.com>
2015-12-06 14:39:23 +0200

    local storage

--- app/Cart/Controller.js
+++ app/Cart/Controller.js
@@ -1,3 +1,13 @@
-app.controller( 'CartController', function( $scope, <Service>) {
+app.controller( 'CartController', function( $scope, CartFactory, $window) {
+	$scope.addToCart = function(id, quant){
+		var product = {'id' : id, 'quantity' : quant};
+		CartFactory.putItem(angular.toJson(product));
+	}
+	
+	$scope.lowerQuantity = function(){
+		if ($scope.quantity != 0)
+			$scope.quantity = $scope.quantity - 1;
+	}
+
 
 });
--- app/Cart/Factory.js
+++ app/Cart/Factory.js
@@ -1,16 +1,23 @@
-CartModule.factory( 'Cart', function CartFectory( $window, $rootScope ) {
-	return {
-		putItem: function( itemId ) {
-			 $window.localStorage && $window.localStorage.setItem('cart-storage', itemId );
-			 return this;
-		},
-		getAllCart: function () {
-			return $window.localStorage && $window.localStorage.getItem('cart-storage');
-			
+CartModule.factory( 'CartFactory', ['$window', '$rootScope' , function($window, $rootScope) {
+	var CartFactory = {};
+	
+	
+	CartFactory.putItem = function( itemId ){
+		if ($window.localStorage.getItem('cart-storage')){
+				$window.localStorage && $window.localStorage.setItem('cart-storage', $window.localStorage.getItem('cart-storage') + itemId );
 		}
-		
+		else {
+			$window.localStorage && $window.localStorage.setItem('cart-storage', itemId );
+		}
+		return this;
 	}
 	
+	CartFactory.getAllCart = function(){
+		return $window.localStorage && $window.localStorage.getItem('cart-storage');
+	}
+
+
 	
+	return CartFactory;
 	
 }]);
