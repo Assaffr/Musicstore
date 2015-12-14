f36d0a4dd857cdec54e196298422dad40522ec2b
Assaffr <djassaf_am@yahoo.com>
2015-12-14 17:53:54 +0200

    WTF!? Look what I have to do for the password verification/...

---
+++ app/Register/RegisterDirective.js
@@ -0,0 +1,34 @@
+RegisterModule.directive("passwordVerify", function() {
+   return {
+      require: "ngModel",
+      scope: {
+        passwordVerify: '='
+      },
+      link: function(scope, element, attrs, RegisterController) {
+        scope.$watch(function() {
+            var combined;
+
+            if (scope.passwordVerify || RegisterController.$viewValue) {
+               combined = scope.passwordVerify + '_' + RegisterController.$viewValue; 
+            }                    
+            return combined;
+            
+        },
+        
+        function (value) {
+            if (value) {
+                RegisterController.$parsers.unshift(function(viewValue) {
+                    var origin = scope.passwordVerify;
+                    if (origin !== viewValue) {
+                        RegisterController.$setValidity("passwordVerify", false);
+                        return undefined;
+                    } else {
+                        RegisterController.$setValidity("passwordVerify", true);
+                        return viewValue;
+                    }
+                });
+            }
+        });
+     }
+   };
+});
